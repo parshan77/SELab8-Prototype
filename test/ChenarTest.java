20b7e73068cc9db472fed123b35424a14fbc9cd2
parshan77 <parshanteimouri@yahoo.com>
2022-06-05 22:28:32 +0430

    ChenarTest implemented.

--- test/ChenarTest.java
+++ test/ChenarTest.java
@@ -4,5 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 class ChenarTest {
+    @Test
+    void testCreation() {
+        Tree tree1 = new Chenar(100);
 
+        assertTrue(tree1.clone() instanceof   Tree);
+    }
+
+    @Test
+    void testAge() {
+        Tree tree1 = new Chenar(100);
+        Tree tree2 = (Tree) tree1.clone();
+        assertTrue(tree2.age ==  tree1.age);
+    }
 }
